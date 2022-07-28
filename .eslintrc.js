2c36951a8e0f9342636b2a64346fb987f989f9a0
shumhoyin.dev <shumhoyin.dev@gmail.com>
2022-07-28 15:13:50 +0800

    eslintrc config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: 'off',
+    semi: 'off',
+    'comma-dangle': 'off',
+    'vue/multi-word-component-names': 'off',
+
   },
 };
