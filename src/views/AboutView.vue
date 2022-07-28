c60594253e4d72384b3a4ea8aff1d31a1f5790b3
shumhoyin.dev <shumhoyin.dev@gmail.com>
2022-07-28 15:13:44 +0800

    setup up a basic vee-validation

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,16 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <Card></Card>
 </template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    Card,
+  },
+};
+</script>
